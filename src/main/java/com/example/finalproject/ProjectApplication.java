9b67fa15487303e5352c0f7296f1cd83cfee22b5
foopzern <jricobantillo4@gmail.com>
2026-05-26 21:20:03 +0800

    made file path folders and added fxmls

--- src/main/java/com/example/finalproject/ProjectApplication.java
+++ src/main/java/com/example/finalproject/ProjectApplication.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class ProjectApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ProjectApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(ProjectApplication.class.getResource("views/user/MainPortal.fxml"));
+        
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Campus Lost and Found System"); 
         stage.setScene(scene);
         stage.show();
     }
